2bf55d90819d02608429d9b22b0d18d82930fc60
Pavithran24 <pavithrannarayan24@gmail.com>
2025-04-18 14:31:11 +0530

    Update admin.py

--- prroject1dj/shop/admin.py
+++ prroject1dj/shop/admin.py
@@ -3,8 +3,8 @@ from .models import Catagory
 from .models import Product
 # from .models import * we can use this command also 
 
-# class CategoryAdmin(admin.ModelAdmin):
-#     list_display=('name','image','description')
+class CategoryAdmin(admin.ModelAdmin):
+  list_display=('name','image','description')
 # admin.site.register(Catagory,CategoryAdmin)
 
 admin.site.register(Catagory)
